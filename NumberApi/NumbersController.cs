628d130ba3f97df8c99779d8459b148ec12eef13
satish venkatakrishnan <svenkatakris@vmware.com>
2020-12-24 13:52:59 +0530

    Add project files.

---
+++ NumberApi/NumbersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumberApi
+{
+    [Route("api/numbers")]
+    [ApiController]
+    public class NumbersController : ControllerBase
+    {
+        private static Random _Random = new Random();
+
+        [HttpGet]
+        public IActionResult GetNumber()
+        {
+            var n = _Random.Next(0, 100);
+            return Ok(n);
+        }
+    }
+}
